c31e8bcd31bc4ba72ce18b723556b3139c6d4cb6
Adeola Kola-Olaleye <inboxadeola@gmail.com>
2021-02-10 12:55:39 +0100

    made new dropdown changes

--- my-website/pages/index.js
+++ my-website/pages/index.js
@@ -1,22 +1,14 @@
 import Head from "next/head";
-import Link from "next/link";
-import Image from "next/image";
-import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
-import Launch from "@material-ui/icons/Launch";
-import {
-  Media,
-  Navbar,
-  Nav,
-  NavDropdown,
-  Container,
-  Row,
-} from "react-bootstrap";
+// import Image from "next/image";
+import Media from "react-bootstrap/Media";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
-import useState from "react";
 import React from "react";
 export default function Home() {
-  const [show, setShow] = React.useState(false);
   const [tab, setTab] = React.useState("Home");
 
   return (
@@ -25,51 +17,35 @@ export default function Home() {
         <title>Adeola's cloud - Software Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       {/* 83B3F3 8AB4F8 E0E0E0 */}
-      {/* <div className="navspacing"> */}
       <Navbar
         style={{ backgroundColor: "#202124", position: "sticky" }}
         variant="dark"
         fixed="top"
       >
-        {/* <Navbar.Brand>H</Navbar.Brand> */}
         <Nav
           fill
-          // className="mr-auto"
           className="justify-content-end"
           defaultActiveKey="Home"
           onSelect={(selectedKey) => setTab(selectedKey)}
         >
-          <Nav.Link href="#Home" defaultValue="Home" eventKey="Home">
+          <Nav.Link defaultValue="Home" eventKey="Home">
             Home
           </Nav.Link>
           <Nav.Link
-            href="#Featured"
+            // href="#Featured"
             defaultValue="Featured"
             eventKey="Featured"
           >
             Featured
           </Nav.Link>
         </Nav>
-        <Nav fill className="mr-auto" defaultActiveKey="home">
-          <Link href="https://github.com/adeolaex/adeolaex/tree/master/my-website">
-            <IconButton aria-label="launch" style={{ color: "#9AA0A6" }}>
-              <Launch fontSize="small" />
-            </IconButton>
-          </Link>
-        </Nav>
-        <Media>
-          <Image
-            className="avatar"
-            src="/avatar.jpg" // Route of the image file
-            height={35} // Desired size with correct aspect ratio
-            width={35} // Desired size with correct aspect ratio
-            alt="Adeola Damilola Kola-Olaleye"
-          />
-        </Media>
+        <Nav fill className="mr-auto" defaultActiveKey="home"></Nav>
+        <Col xs={0} md={-1}>
+          <Image src="/avatar.jpg" roundedCircle height={42} width={42} />
+        </Col>
       </Navbar>
-      {/* </div> */}
+
       <main className="main">
         <h1 className="title">
           {tab == "Home" ? (
@@ -101,7 +77,9 @@ export default function Home() {
               beautiful, fast, and open through great typography.
             </a>
           ) : (
-            <a>Featured</a>
+            <Col xs={10} md={12}>
+              <Image src="/featured.png" fluid />
+            </Col>
           )}
         </h1>
       </main>
@@ -131,10 +109,6 @@ export default function Home() {
           line-height: 1.35;
           font-size: 3.5rem;
           display: block;
-        }
-        .navspacing {
-          padding-right: 2.5em;
-          padding-left: 2.5em;
         }
       `}</style>
 
